a91f03709f18fa5fedb8d8d9657417022a8e13ce
Chilla-ia <manon.touchard@epsi.fr>
2022-02-23 10:41:50 +0100

    starting project

---
+++ src/main/java/com/autopieces/autopieces/AutopiecesApplication.java
@@ -0,0 +1,13 @@
+package com.autopieces.autopieces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopiecesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopiecesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autopieces/autopieces/AutopiecesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopieces.autopieces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopiecesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
