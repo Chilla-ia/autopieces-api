1a79f1b0be38934a0ea447da32d9fdb491476d3a
Chilla-ia <manon.touchard@epsi.fr>
2022-02-23 11:47:22 +0100

    add hello world and readme file

--- src/main/java/com/autopieces/autopieces/AutopiecesApplication.java
+++ src/main/java/com/autopieces/autopieces/AutopiecesApplication.java
@@ -1,13 +1,19 @@
 package com.autopieces.autopieces;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutopiecesApplication {
+public class AutopiecesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutopiecesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
 }
+
